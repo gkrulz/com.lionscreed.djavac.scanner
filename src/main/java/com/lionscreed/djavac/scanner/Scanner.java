5c85bcbef60c5a743474113f98ecc4446e73684c
Padmaka Wijayagoonawardena <padmaka.jw@gmail.com>
2015-09-09 17:54:41 +0530

    further scanner regex dev

--- src/main/java/com/lionscreed/djavac/scanner/Scanner.java
+++ src/main/java/com/lionscreed/djavac/scanner/Scanner.java
@@ -5,17 +5,45 @@ import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Padmaka on 9/8/2015.
  */
 public class Scanner {
-    public Collection scan(){
+    public Collection read(){
         File file = new File(System.getProperty("user.dir")+"\\src\\main\\resources");
         System.out.println(System.getProperty("user.dir")+"\\src\\main\\resources");
         System.out.println(file.exists() ? "yes" : "NO");
         Collection files = FileUtils.listFiles(file, new RegexFileFilter("^(.*?)"), DirectoryFileFilter.DIRECTORY);
         return files;
+    }
+
+    public void findDependencies(ArrayList<File> files) throws IOException {
+        for(File f : files){
+            System.out.println("------------------------------------------------------");
+            System.out.println(f.getName());
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(f));
+
+            String fileString = FileUtils.readFileToString(f, inputStreamReader.getEncoding());
+//            Pattern pattern = Pattern.compile("(public|private|abstract)\\s+(class|interface)\\s+\\w+((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+( ,\\w+)*)|(\\s+extends\\s+\\w+\\s+implements\\s+\\w+( ,\\w+)*))?\\s*\\{");
+//            Pattern pattern = Pattern.compile("((public|private|abstract)\\s+(class|interface)\\s+\\w+((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+( ,\\w+)*)|(\\s+extends\\s+\\w+\\s+implements\\s+\\w+( ,\\w+)*))?\\s*\\{)|((class|interface)\\s+\\w+((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+( ,\\w+)*)|(\\s+extends\\s+\\w+\\s+implements\\s+\\w+( ,\\w+)*))?\\s*\\{)");
+            Pattern pattern = Pattern.compile("((public|protected|private)?\\s*class\\s+(\\w+)((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+\\s*(,\\s*\\w+)*))?\\s*)?\\{([^{}]|(\\{([^{}])*\\}))*\\}");
+            Matcher matcher = pattern.matcher(fileString);
+            while(matcher.find()){
+                try {
+                    System.out.println(matcher.group());
+                }catch (IllegalStateException e){
+                    System.err.println(f.getName() + " - No match found");
+                }
+            }
+            System.out.println("------------------------------------------------------");
+        }
     }
 }
--- src/test/java/com/lionscreed/djavac/scanner/test/TestScanner.java
+++ src/test/java/com/lionscreed/djavac/scanner/test/TestScanner.java
@@ -1,12 +1,10 @@
 package com.lionscreed.djavac.scanner.test;
 
 import com.lionscreed.djavac.scanner.Scanner;
-import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Created by Padmaka on 9/8/2015.
@@ -14,9 +12,8 @@ import java.util.regex.Pattern;
 public class TestScanner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner();
-        Collection fileCollection = scanner.scan();
+        Collection fileCollection = scanner.read();
         ArrayList<File> files = new ArrayList<File>();
-        TestScanner testScanner = new TestScanner();
 
         for(Object obj :fileCollection){
             File f = (File)obj;
@@ -24,30 +21,9 @@ public class TestScanner {
         }
 
         try {
-            testScanner.findDependencies(files);
+            scanner.findDependencies(files);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public void findDependencies(ArrayList<File> files) throws IOException {
-        for(File f : files){
-            System.out.println("------------------------------------------------------");
-            System.out.println(f.getName());
-            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(f));
-
-            String fileString = FileUtils.readFileToString(f, inputStreamReader.getEncoding());
-            Pattern pattern = Pattern.compile("(public|private|abstract)\\s+(class|interface)\\s+\\w+((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+( ,\\w+)*)|(\\s+extends\\s+\\w+\\s+implements\\s+\\w+( ,\\w+)*))?\\s*\\{");
-
-            Matcher matcher = pattern.matcher(fileString);
-            System.out.println(matcher.find());
-            try {
-                System.out.println(matcher.group());
-                System.out.println("------------------------------------------------------");
-            }catch (IllegalStateException e){
-                e.printStackTrace();
-            }
-
         }
     }
 }
