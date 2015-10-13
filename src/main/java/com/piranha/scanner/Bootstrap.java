69ff00656770eda1c22ea6579cbc8fbfcea969d9
padmaka <padmaka.jw@gmail.com>
2015-10-14 02:00:57 +0530

    Change bootstrap 1.0

--- src/main/java/com/piranha/scanner/Bootstrap.java
+++ src/main/java/com/piranha/scanner/Bootstrap.java
@@ -4,5 +4,7 @@ package com.piranha.scanner;
  * Created by Padmaka on 8/25/2015.
  */
 public class Bootstrap {
-    
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
