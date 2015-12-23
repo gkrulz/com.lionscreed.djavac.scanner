06776390bb96fae0cb2af29ab2235b8ce4113d80
Padmaka <padmaka.jw@gmail.com>
2015-12-23 17:04:46 +0530

    Scanner - added feature to get a list of all the classes (even the ones in same java file)

--- src/main/java/com/piranha/scanner/Scanner.java
+++ src/main/java/com/piranha/scanner/Scanner.java
@@ -3,7 +3,6 @@ package com.piranha.scanner;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
-import com.piranha.scanner.model.JavaClass;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
@@ -24,15 +23,16 @@ import java.util.regex.Pattern;
  */
 public class Scanner {
     private static final Logger log = Logger.getLogger(Scanner.class);
-    private ArrayList<JavaClass> javaClasses;
+    private ArrayList<JsonObject> classes;
+    private ArrayList<String> classNames;
 
-    public Scanner(){
-        javaClasses = new ArrayList<JavaClass>();
+    public Scanner() {
+        classes = new ArrayList<JsonObject>();
     }
 
-    public Collection read(){
+    public Collection read() {
         log.debug(System.getProperty("user.dir"));
-        File file = new File(System.getProperty("user.dir")+"/src/main/resources");
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources");
         Collection files = FileUtils.listFiles(file, new RegexFileFilter("^(.*?)"), DirectoryFileFilter.DIRECTORY);
         return files;
     }
@@ -40,40 +40,29 @@ public class Scanner {
     public void findDependencies(ArrayList<File> files) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        for(File f : files){
+        for (File f : files) {
 
-            log.debug(f.getName());
+//            log.debug(f.getName());
             InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(f));
 
             String fileString = FileUtils.readFileToString(f, inputStreamReader.getEncoding());
 
             JsonObject classJson = this.getClass(fileString);
 
-                try {
-                    if (classJson != null) {
-//                        log.debug(classJson.get("classDeclaration").getAsString());
-//                    log.debug(matcher.end());
-//                    log.debug(fileString.charAt(matcher.end() - 1));
-                        this.findOutterClasses(f.getName(), classJson.get("classDeclaration").getAsString(), fileString, classJson.get("end").getAsInt() - 1);
-//                    String str = fileString.substring(classJson.get("end").getAsInt(), endOfClass);
-//                    JavaClass javaClass = new JavaClass();
-//                    javaClass.setDeclaration(classJson.get("classDeclaration").getAsString());
-//                    javaClass.setBody(str);
-//                    javaClasses.add(javaClass);
-//                    log.debug("------------------------------------------------------");
-//                    log.debug(str);
-//                    log.debug("------------------------------------------------------");
-                    }
-                }catch (IllegalStateException e){
-                    log.error(f.getName() + " - No match found");
+            try {
+                if (classJson != null) {
+                    this.findOutterClasses(f, classJson.get("classDeclaration").getAsString(), fileString, classJson.get("end").getAsInt() - 1);
                 }
+            } catch (IllegalStateException e) {
+                log.error(f.getName() + " - No match found");
+            }
 
 
         }
         log.debug(gson.toJson(classes));
     }
 
-    public JsonObject getClass(String fileString){
+    public JsonObject getClass(String fileString) {
 
         Pattern pattern = Pattern.compile("(((public|protected|private|)?(\\s+abstract)?(\\s+static)?\\s+class\\s+(\\w+)((\\s+extends\\s+\\w+)|(\\s+implements\\s+\\w+\\s*(,\\s*\\w+\\s*)*)|(\\s+extends\\s+\\w+\\s+implements\\s+\\w+\\s*(,\\s*\\w+\\s*)*))?\\s*\\{)|" +
                 "((public|protected)?(\\s+abstract)?(\\s+static)?\\s+interface\\s+(\\w+)(\\s+extends\\s+\\w+\\s*(,\\s*\\w+\\s*)*)?\\s*\\{))");
@@ -83,9 +72,26 @@ public class Scanner {
         JsonObject classJson = null;
         try {
             matcher.find();
+            String classDeclaration = matcher.group();
+
             classJson = new JsonObject();
             classJson.addProperty("end", matcher.end());
-            classJson.addProperty("classDeclaration", matcher.group());
+            classJson.addProperty("classDeclaration", classDeclaration);
+            String className = classDeclaration.replace("public", "");
+            className = className.replace("protected", "");
+            className = className.replace("private", "");
+            className = className.replace("abstract", "");
+            className = className.replace("static", "");
+            className = className.replace("class", "");
+            className = className.replace("interface", "");
+            className = className.replace("extends", "");
+            className = className.replace("implements", "");
+            className = className.replace("{", "");
+            className = className.trim();
+            String[] classNameParts = className.split(" ");
+            className = classNameParts[0];
+            log.debug("\""+className+"\"");
+//            classNames.add();
         } catch (IllegalStateException e) {
             return null;
         }
@@ -94,9 +100,7 @@ public class Scanner {
         return classJson;
     }
 
-    private ArrayList<JsonObject> classes = new ArrayList<JsonObject>();
-
-    public void findOutterClasses(String fileName, String className, String fileString, int startOfClass){
+    public void findOutterClasses(File file, String className, String fileString, int startOfClass) {
 
         int endOfClass = 0;
         Stack<Character> stack = new Stack<Character>();
@@ -113,25 +117,25 @@ public class Scanner {
                 char last = stack.peek();
                 if (current == '}' && last == '{') {
                     stack.pop();
-                }
-                else{
+                } else {
                     endOfClass = -1;
                 }
 
-                if (stack.isEmpty()){
+                if (stack.isEmpty()) {
 //                    endOfClass = i;
-                    String classString = fileString.substring(startOfClass+1, i);
+                    String classString = fileString.substring(startOfClass + 1, i);
                     JsonObject classJson = new JsonObject();
-                    classJson.addProperty("file", fileName);
+                    classJson.addProperty("file", file.getName());
+                    classJson.addProperty("filePath", file.getPath());
                     classJson.addProperty("className", className);
                     classJson.addProperty("classSting", classString);
                     classes.add(classJson);
 
-                    if (i+1 < fileString.length()){
+                    if (i + 1 < fileString.length()) {
                         String restOfTheString = fileString.substring(i, fileString.length());
                         JsonObject nextClass = this.getClass(restOfTheString);
                         if (nextClass != null) {
-                            this.findOutterClasses(fileName, nextClass.get("classDeclaration").getAsString(), restOfTheString, nextClass.get("end").getAsInt() - 1);
+                            this.findOutterClasses(file, nextClass.get("classDeclaration").getAsString(), restOfTheString, nextClass.get("end").getAsInt() - 1);
                         }
                     }
                     break;
